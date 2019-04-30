e5e3e853c1a57e8c59aa4bd48906d8eecb577738
cprejler <prejler93@gmail.com>
2019-04-30 10:05:45 +0200

    Oprettet KonkurrenceSvømmer og Træner class

---
+++ Delfinen/src/businesslogic/Træner.java
@@ -0,0 +1,19 @@
+
+package businesslogic;
+
+import java.util.ArrayList;
+
+
+public class Træner {
+    private String navn;
+    private ArrayList<Medlem> medlemmer;
+
+    public Træner(String navn) {
+        this.navn = navn;
+        medlemmer = new ArrayList<Medlem>();
+        
+    }
+    
+    
+    
+}
