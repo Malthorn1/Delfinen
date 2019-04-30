0d04cc265f9ceeb3888a72b4248666380d4b3e99
Bringordie <48799015+Bringordie@users.noreply.github.com>
2019-04-30 10:35:39 +0200

    Update delfinscript.sql

--- delfinscript.sql
+++ delfinscript.sql
@@ -1,5 +1,6 @@
 DROP TABLE if exists DISCIPLIN;
 DROP TABLE if exists MEDLEM_DISCIPLIN;
+DROP TABLE if exists TRÆNER;
 DROP TABLE if exists MEDLEMMER;
 
 
@@ -9,25 +10,31 @@ CREATE TABLE MEDLEMMER (
     TELEFONNUMMER INTEGER not null,
     MEDLEMSNUMMER INTEGER not null AUTO_INCREMENT,
     DATOOPRETTET TIMESTAMP,
-    BETALT TINYINT,
+    MEDLEMSKAB TINYINT,
     KONKURRENCESVØMMER TINYINT,
-    TRÆNER VARCHAR(30),
     PRIMARY KEY (MEDLEMSNUMMER)
 );
 
-insert into MEDLEMMER (navn, alder, TELEFONNUMMER, MEDLEMSNUMMER, DATOOPRETTET, BETALT, KONKURRENCESVØMMER, TRÆNER)
-values ("Robert", 57, 55555555, 1, now(), 1, 1, "Hans Erik");
+insert into MEDLEMMER (navn, alder, TELEFONNUMMER, MEDLEMSNUMMER, DATOOPRETTET, MEDLEMSKAB, KONKURRENCESVØMMER)
+values ("Robert", 57, 55555555, 1, now(), 1, 1);
+
+CREATE TABLE TRÆNER (
+	træner_ID INTEGER not null AUTO_INCREMENT,
+	træner_navn VARCHAR(30)
+);
+
+insert into TRÆNER (træner_ID, træner_navn) values (1, "Robert Junor");
 
 
 CREATE TABLE DISCIPLIN (
-	navn varchar(15),
+	disciplin_navn varchar(15),
     disciplinID INT NOT NULL
 );
 
-insert INTO DISCIPLIN (navn, disciplinID) values ("Crawl", 1);
-insert INTO DISCIPLIN (navn, disciplinID) values ("Rygcrawl", 2);
-insert INTO DISCIPLIN (navn, disciplinID) values ("Brystsvømning", 3);
-insert INTO DISCIPLIN (navn, disciplinID) values ("Butterfly", 4);
+insert INTO DISCIPLIN (disciplin_navn, disciplinID) values ("Crawl", 1);
+insert INTO DISCIPLIN (disciplin_navn, disciplinID) values ("Rygcrawl", 2);
+insert INTO DISCIPLIN (disciplin_navn, disciplinID) values ("Brystsvømning", 3);
+insert INTO DISCIPLIN (disciplin_navn, disciplinID) values ("Butterfly", 4);
 
 CREATE TABLE MEDLEM_DISCIPLIN (
 bedstetid int,
