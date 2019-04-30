8f9861960c3fabd312d5abdab03f48d48861778c
cprejler <prejler93@gmail.com>
2019-04-30 19:25:19 +0200

    Oprettet FakeUI class

---
+++ Delfinen/src/presentation/FakeUI.java
@@ -0,0 +1,44 @@
+package presentation;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author prejl
+ */
+public class FakeUI implements UI {
+    
+    private String[] input;
+    private int index = 0;
+    public ArrayList<String> output = new ArrayList<>();
+    
+    public FakeUI(String[] input){
+        this.input = input;
+    }
+
+    @Override
+    public void visHovedMenu() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String hovedMenuValg() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void administrerBrugere() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void administrerBetaling() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void opretBrugerDB() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
