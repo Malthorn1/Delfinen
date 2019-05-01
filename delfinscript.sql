cf4dbad5fc2c3d1192b35ec72719fea577758122
Malthorn1 <48799093+Malthorn1@users.noreply.github.com>
2019-05-01 03:26:57 +0200

    Update delfinscript.sql

--- delfinscript.sql
+++ delfinscript.sql
@@ -1,7 +1,7 @@
 DROP TABLE if exists DISCIPLIN;
 DROP TABLE if exists MEDLEM_DISCIPLIN;
-DROP TABLE if exists TRÆNER;
 DROP TABLE if exists MEDLEMMER;
+DROP TABLE if exists TRÆNER;
 
 
 CREATE TABLE TRÆNER (
@@ -35,8 +35,11 @@ CREATE TABLE MEDLEMMER (
     FOREIGN KEY (træner_ID) REFERENCES TRÆNER(træner_ID)
 );
 
-insert into MEDLEMMER (navn, alder, TELEFONNUMMER, MEDLEMSNUMMER, DATOOPRETTET, MEDLEMSKAB, KONKURRENCESVØMMER)
-values ("Robert", 57, 55555555, 1, now(), 1, 1);
+insert into MEDLEMMER (navn, alder, TELEFONNUMMER, MEDLEMSNUMMER, DATOOPRETTET, MEDLEMSKAB, KONKURRENCESVØMMER)  values ("Robert", 57, 55555555, 1, now(), 1, 1);
+insert into MEDLEMMER (navn, alder, TELEFONNUMMER, MEDLEMSNUMMER, DATOOPRETTET, MEDLEMSKAB, KONKURRENCESVØMMER)  values ("Rasmus", 17, 12151212, 0, now(), 1, 1);
+insert into MEDLEMMER (navn, alder, TELEFONNUMMER, MEDLEMSNUMMER, DATOOPRETTET, MEDLEMSKAB, KONKURRENCESVØMMER)  values ("Kasper", 20, 18171981, 0, now(), 0, 1);
+insert into MEDLEMMER (navn, alder, TELEFONNUMMER, MEDLEMSNUMMER, DATOOPRETTET, MEDLEMSKAB, KONKURRENCESVØMMER)  values ("Casper", 17, 30205145, 0, now(), 0, 1);
+
 
 
 CREATE TABLE DISCIPLIN (
@@ -52,10 +55,21 @@ insert INTO DISCIPLIN (disciplin_navn, disciplinID) values ("Butterfly", 4);
 CREATE TABLE MEDLEM_DISCIPLIN (
 bedstetid int,
 MEDLEMSNUMMER int,
-DISCIPLIN int,
+DISCIPLIN VARCHAR(30),
 disciplinID int,
 FOREIGN KEY (MEDLEMSNUMMER) REFERENCES MEDLEMMER(MEDLEMSNUMMER)
 );
+insert INTO MEDLEM_DISCIPLIN (bedstetid, MEDLEMSNUMMER, DISCIPLIN, disciplinID) values (30, 1,"Butterfly", 4);
+insert INTO MEDLEM_DISCIPLIN (bedstetid, MEDLEMSNUMMER, DISCIPLIN, disciplinID) values (50, 3,"Brystsvømning", 3);
+insert INTO MEDLEM_DISCIPLIN (bedstetid, MEDLEMSNUMMER, DISCIPLIN, disciplinID) values (20, 2,"Brystsvømning", 3);
+insert INTO MEDLEM_DISCIPLIN (bedstetid, MEDLEMSNUMMER, DISCIPLIN, disciplinID) values (30, 1,"Crawl", 1);
+insert INTO MEDLEM_DISCIPLIN (bedstetid, MEDLEMSNUMMER, DISCIPLIN, disciplinID) values (25, 1,"Brystsvømning", 3);
+insert INTO MEDLEM_DISCIPLIN (bedstetid, MEDLEMSNUMMER, DISCIPLIN, disciplinID) values (99, 1,"Brystsvømning", 3);
+insert INTO MEDLEM_DISCIPLIN (bedstetid, MEDLEMSNUMMER, DISCIPLIN, disciplinID) values (26, 1,"Brystsvømning", 3);
+insert INTO MEDLEM_DISCIPLIN (bedstetid, MEDLEMSNUMMER, DISCIPLIN, disciplinID) values (25, 1,"Brystsvømning", 3);
+
+
+
 
 
 commit;
